0d59b92044511e255ffab38e648c4175cef9394a
Oliver Schmid <oliver.schmid@cscs.ch>
2025-08-26 12:58:13 +0200

    remove the fallback to sid as a user id since its incorrect to do so

--- services/marmotgraph-authentication/src/main/java/org/marmotgraph/authentication/keycloak/KeycloakController.java
+++ services/marmotgraph-authentication/src/main/java/org/marmotgraph/authentication/keycloak/KeycloakController.java
@@ -116,7 +116,7 @@ public class KeycloakController {
 
     public User buildUserInfoFromKeycloak(Map<String, Claim> claims) {
         Claim userName = claims.get(PREFERRED_USERNAME);
-        Claim nativeId = claims.containsKey("sub") ? claims.get("sub") : claims.get("sid");
+        Claim nativeId = claims.get("sub");
         Claim name = claims.get("name");
         Claim givenName = claims.get("given_name");
         Claim familyName = claims.get("family_name");
